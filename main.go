114841126cc4a09d0ec62a4dc27dc9de5497dfaf
Rozita <rozita.teim@gmail.com>
2019-02-13 02:15:44 -0800

    update main

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 
 func main() {
 	fmt.Println("hello world!")
-	/* Initialize MPT and stack */
-	mpt := p1.InitializeMpt()
-	//s := p1.InitializeStack()
+	/* Initialize MPT */
+	  mpt := p1.InitializeMpt()
 
-	/* Test GetRoot */
-	//mpt.GetRootNode()
+
 
  /* Test Cases */
-		p1.Test_compact_encode()
-		mpt.Test_Get()
-	 //p1.Test_Insert_Get()
+		 p1.TestCompact()
+		 mpt.Test_Get()
+	   p1.Test_Insert_Get_Delete()
 	 
 	
  

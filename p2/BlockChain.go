414fbe3e6f099c48eb0e2e8c5a3f6bb7afe32014
Rozita <rozita.teim@gmail.com>
2019-03-03 01:45:05 -0800

    Initialization

--- p2/BlockChain.go
+++ p2/BlockChain.go
@@ -1,10 +1,25 @@
 package p2
 
-
 /*-------------------------STRUCT---------------------------------------------------*/
 /* Struct data structure for variables
 /*-------------------------STRUCT---------------------------------------------------*/
+
+/* BlockChain struct
+*
+* To Define blockChain variables
+*
+ */
 type BlockChain struct {
 	Chain  map[int32][]Block `json:"chain"`
 	Length int32             `json:"length"`
+}
+
+/* Initial
+*
+* To Initialize blockChain
+*
+ */
+func (blockChain *BlockChain) Initial() {
+	blockChain.Chain = make(map[int32][]Block)
+	blockChain.Length = 0
 }

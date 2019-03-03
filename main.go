311c4bb81d0b1fc32de8a3b3c7a3b20288013425
Rozita <rozita.teim@gmail.com>
2019-03-03 14:02:56 -0800

    update main

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"MerklePatriciaTree/p1"
 	"fmt"
+	//"MerklePatriciaTree/p1"
+	"MerklePatriciaTree/p2"
 )
 
 func main() {
 	fmt.Println("hello world!")
 
- /* Test Cases */
-		  p1.TestCompact()
-			p1.Test1_2()
-			p1.Test3_4()
-			p1.Test5()
-			p1.Test6()
-			p1.Test7()
+	/* Test Cases  for project 1*/
+	// p1.TestCompact()
+	// p1.Test1_2()
+	// p1.Test3_4()
+	// p1.Test5()
+	// p1.Test6()
+
+
+
+mpt := p2.MerklePatriciaTrie{}
+mpt.Initial()
+block := p2.Block{}
+blockChain := p2.BlockChain{}
+blockChain.Initial()
+fmt.Printf("mpt:", mpt)
+fmt.Printf("block:", block)
+fmt.Printf("blockChain:", blockChain)
 }
--- p2/Block.go
+++ p2/Block.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
-
 	"golang.org/x/crypto/sha3"
 )
 

fcc42d27e042221926194b80709f7f75d8ad5e53
Rozita <rozita.teim@gmail.com>
2019-03-02 23:58:16 -0800

    implement json converter

--- p2/Block.go
+++ p2/Block.go
@@ -54,8 +54,35 @@ func (block *Block) Initial(value MerklePatriciaTrie, height int32, parentHash s
 }
 
 /*-------------------------JSON HELPER---------------------------------------------------*/
-/* Data initialization
+/* JSON feature
 /*-------------------------JSON HELPER---------------------------------------------------*/
+
+/* UnmarshalJSON
+*
+* To encodes a block instance into a JSON format string
+* @input: an instanse of block
+* @output: a string of JSON format
+*
+ */
+func (block *Block) UnmarshalJSON(data []byte) error {
+	JsonBlock := SymmetricJsonBlock{}
+	err := json.Unmarshal(data, &JsonBlock)
+	if err != nil {
+		return err
+	}
+	block.Header.Height = JsonBlock.Height
+	block.Header.Timestamp = JsonBlock.Timestamp
+	block.Header.Hash = JsonBlock.Hash
+	block.Header.ParentHash = JsonBlock.ParentHash
+	block.Header.Size = JsonBlock.Size
+	mpt := MerklePatriciaTrie{}
+	mpt.Initial()
+	for k, v := range JsonBlock.Mpt {
+		mpt.Insert(k, v)
+	}
+	block.Value = mpt
+	return nil
+}
 
 /* EncodeToJson
 *
@@ -80,19 +107,6 @@ func (block *Block) DecodeFromJson(jsonString string) error {
 	return json.Unmarshal([]byte(jsonString), block)
 }
 
-/* Insert
-*
-* To insert key/value pair into the main MPT as block and
-* update the mpt size and hash value. take a string that represents the JSON value of a block as
-* @input:  key string, value string
-* @output: update block
-*
- */
-func (block *Block) Insert(key string, value string) {
-	block.Value.Insert(key, value)
-	block.UpdateMpt()
-}
-
 /* MarshalJSON
 *
 * To hash MPT with the SHA3-256 encoded value of this string and update MPT value upon the
@@ -112,6 +126,37 @@ func (block *Block) MarshalJSON() ([]byte, error) {
 	})
 }
 
+/* SymmetricJsonBlock
+*
+* Symmetric Json struct for Block struct
+*
+ */
+type SymmetricJsonBlock struct {
+	Size       int32             `json:"size"`
+	ParentHash string            `json:"parentHash"`
+	Height     int32             `json:"height"`
+	Timestamp  int64             `json:"timeStamp"`
+	Hash       string            `json:"hash"`
+	Mpt        map[string]string `json:"mpt"`
+}
+
+/*-------------------------MASTER---------------------------------------------------*/
+/* Function to modify Block
+/*-------------------------MASTER---------------------------------------------------*/
+
+/* Insert
+*
+* To insert key/value pair into the main MPT as block and
+* update the mpt size and hash value. take a string that represents the JSON value of a block as
+* @input:  key string, value string
+* @output: update block
+*
+ */
+func (block *Block) Insert(key string, value string) {
+	block.Value.Insert(key, value)
+	block.UpdateMpt()
+}
+
 /* UpdateMpt
 *
 * To hash MPT with the SHA3-256 encoded value of this string and update MPT value upon the
@@ -125,18 +170,4 @@ func (block *Block) UpdateMpt() {
 	hashConverter := sha3.New256()
 	hashStr := string(block.Header.Height) + string(block.Header.Timestamp) + block.Header.ParentHash + block.Value.root + string(block.Header.Size)
 	block.Header.Hash = hex.EncodeToString(hashConverter.Sum([]byte(hashStr)))
-}
-
-/* SymmetricJsonBlock
-*
-* Symmetric Json struct for Block struct
-*
- */
-type SymmetricJsonBlock struct {
-	Size       int32             `json:"size"`
-	ParentHash string            `json:"parentHash"`
-	Height     int32             `json:"height"`
-	Timestamp  int64             `json:"timeStamp"`
-	Hash       string            `json:"hash"`
-	Mpt        map[string]string `json:"mpt"`
 }
